1f6b5fc46ddbaacc0000bf75f004c56b8ec1cdc8
maximealbert <maxime.albert@epitech.digital>
2023-09-05 15:34:47 +0200

    End ex_03bis

--- ex_04.py
+++ ex_04.py
@@ -14,6 +14,13 @@ class Budget():
 
         print(self.__transactions)
 
+        print("Welcome to your app, here's your transactions history")
+        categorieslist = list(self.__transactions.keys())
+        for category in categorieslist:
+            print(f'Element in this category : {category}')
+            for element in self.__transactions[category]:
+                print(element)
+
     __transactions = dict()
 
     def print_transactions(self, category="defaultvalue"):
@@ -27,19 +34,8 @@ class Budget():
         else:
             print(self.__transactions[category])
 
-    def get_categories(self):
-        categorieslist = list(self.__transactions.keys())
-        print(categorieslist)
 
-    def print_sorted_transactions(self, category="defaultvalue"):
-        finallist = []
-        categorieslist = list(self.__transactions.keys())
-        if category == "defaultvalue":
-            for cat in categorieslist:
-                finallist = finallist + self.__transactions[cat]
-            print(sorted(finallist))
-        else:
-            print(sorted(self.__transactions[category]))
+
 
     def add_transactions(self, categoryfromheader, valuefromheader):
         f = open(self.filepath)
@@ -71,10 +67,39 @@ class Budget():
             # write
             file.write(newData)
 
+    ## Code to get the balance of the user
+    def getsolde (self):
+        finallist = []
+        solde = 0
+        categorieslist = list(self.__transactions.keys())
+
+        for cat in categorieslist:
+            finallist = finallist + self.__transactions[cat]
+        print(finallist)
+        for number in finallist:
+            solde = solde + number
+
+        print(f'Your balance is : {solde} ')
+
+
+
+
+    ## Code to get transactions history at first launch
+    def firstlaunch(self):
+        print("Welcome to your app, here's your transactions history")
+        categorieslist = list(self.__transactions.keys())
+        for category in categorieslist:
+            print(f'Element in this category : {category}')
+            for element in self.__transactions[category]:
+                print(element)
+
+
 
 if __name__ == '__main__':
     myBudget = Budget('data.json')
     # myBudget.get_categories()
-    myBudget.print_transactions()
+    # myBudget.print_transactions()
     #myBudget.print_sorted_transactions('income')
     #myBudget.add_transactions([-12, -102.13], "corn for poney")
+    #myBudget.firstlaunch()
+    #myBudget.getsolde()
